102b6b5ea4a9fae106d3714084fd56f01b30b3d4
justsealan <justsealan@gmail.com>
2022-01-19 20:35:44 +0000

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-face">
+      <h1>High Scores per Country</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './index.css';
+import './fonts/arcadeclassic/ARCADECLASSIC.TTF'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
